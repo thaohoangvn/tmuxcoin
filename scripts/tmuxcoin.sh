fc8284343357f11e7a5dcf91a59890f46baf4636
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-18 09:58:47 +0700

    [f] Use cron job

--- scripts/tmuxcoin.sh
+++ scripts/tmuxcoin.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
-source $HOME/.icos.sh
+cat ~/p.dat; return
 
-get_coin() {
-  TICKER_DATA=`curl --request GET --url https://api.bitfinex.com/v1/pubticker/${1}usd | jq '.mid'`
-  echo -n " $1: $TICKER_DATA |" >> p.dat
-}
-
-get_coins() {
-  rm p.dat
-  for ico in $ICOS; do
-    get_coin $ico &
-  done; wait
-  cat p.dat; return
-}
-
-get_coins
